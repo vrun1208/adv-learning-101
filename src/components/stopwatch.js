c7d89e5d4db69dce0dc039a669d0b8c59f1d0ae1
vrun1208 <vrun4750@gmail.com>
2023-09-22 02:04:05 +0530

    stopwatch-done

--- src/components/stopwatch.js
+++ src/components/stopwatch.js
@@ -1,24 +1,55 @@
 import React ,{ useEffect, useState } from "react"
 
 const StopWatch = () => {
-    const [hrs, setHrs] = useState(0);
-    const [min, setMin] = useState(0);
-    const [sec, setsec] = useState(0);
+    const [timer, setTimer] = useState({
+        hrs: 0,
+        min: 0,
+        sec: 0,
+    });
 
-    useEffect(() => {
-        const timeInSec = setTimeout(() =>{
-            setsec(sec+1);
-        }, 1000)
-        const timeInMin = setTimeout(() =>{
-            setMin(min+1);
-            //setsec(() => 0)
-        }, 60*1000)
-    },[sec, min])
+    const [isRun, setIsRun] = useState();
 
+    const StartOrStop = () => {
+        if(!isRun){
+            let ss = setInterval((formatTime), 1000);
+            setIsRun(ss);
+        }
+        else{
+            clearInterval(isRun);
+            setIsRun("");
+        }
+    }
+
+    const formatTime = () => {
+        setTimer((e) => {
+            let newTime = { ...e };
+            if(newTime.sec < 59) newTime.sec += 1;
+            else{
+                newTime.min += 1;
+                newTime.sec = 0;
+            }
+
+            if(newTime.min === 60){
+                newTime.min = 0;
+                newTime.hrs += 1;
+            }
+            return newTime;
+        })
+      }
+
+      const reset = () => {
+        clearInterval(isRun);
+        setTimer({
+            hrs:0, min:0, sec:0
+        })
+      }
 
     return(
         <div>
-            {hrs} : {min} : {sec}
+           <h1>STOPWATCH</h1>
+            <h2>{timer.hrs < 10 ? 0: ""}{timer.hrs} : {timer.min < 10 ? 0: ""}{timer.min} : {timer.sec < 10 ? 0: ""}{timer.sec}</h2>
+            <button onClick={StartOrStop}>START/STOP</button>
+            <button onClick={reset}>RESET</button>
         </div>
     )
 }
